d104b73f5a3feda3e48ee34b3be7147fb98ad5b8
Oscar Söderlund <odsod@users.noreply.github.com>
2019-07-30 19:24:19 +0200

    Inline SphericalPointCloud.parseBlock
    Part of ongoing simplification effort.

--- spherical_point_cloud.go
+++ spherical_point_cloud.go
@@ -22,35 +22,31 @@ func (s *SphericalPointCloud) UnmarshalPacket(packet *Packet) {
 	// duration is in nanoseconds and Velodyne timestamp in microseconds
 	s.Timestamp = time.Duration(packet.Timestamp) * time.Microsecond
 	for i := 0; i < len(packet.Blocks); i++ {
-		s.parseBlock(i, packet)
-	}
-}
-
-func (s *SphericalPointCloud) parseBlock(blockIndex int, packet *Packet) {
-	azimuth := packet.Blocks[blockIndex].Azimuth
-	timingOffsets := calculateTimingOffset(packet.ReturnMode)
-	for j := 0; j < len(packet.Blocks[0].Channels); j++ {
-		if j == 16 {
-			azimuth = interpolateAzimuth(blockIndex, packet)
-		}
-		distance := packet.Blocks[blockIndex].Channels[j].Distance
-		lastReturn := false
-		switch packet.ReturnMode {
-		case ReturnModeDualReturn:
-			// Even number blocks (0,2,4,...) contain last return
-			if blockIndex%2 == 0 {
+		azimuth := packet.Blocks[i].Azimuth
+		timingOffsets := calculateTimingOffset(packet.ReturnMode)
+		for j := 0; j < len(packet.Blocks[0].Channels); j++ {
+			if j == 16 {
+				azimuth = interpolateAzimuth(i, packet)
+			}
+			distance := packet.Blocks[i].Channels[j].Distance
+			lastReturn := false
+			switch packet.ReturnMode {
+			case ReturnModeDualReturn:
+				// Even number blocks (0,2,4,...) contain last return
+				if i%2 == 0 {
+					lastReturn = true
+				}
+			case ReturnModeLastReturn:
 				lastReturn = true
 			}
-		case ReturnModeLastReturn:
-			lastReturn = true
+			point := SphericalPoint{}
+			point.Distance = float64(distance) * distanceFactor
+			point.Azimuth = deg2Rad(float64(azimuth) * azimuthFactor)
+			point.Elevation = verticalAngle(j)
+			point.Reflectivity = packet.Blocks[i].Channels[j].Reflectivity
+			point.LastReflection = lastReturn
+			point.TimingOffset = timingOffsets[j][i]
+			s.SphericalPoints[i*ChannelsPerBlock+j] = point
 		}
-		point := SphericalPoint{}
-		point.Distance = float64(distance) * distanceFactor
-		point.Azimuth = deg2Rad(float64(azimuth) * azimuthFactor)
-		point.Elevation = verticalAngle(j)
-		point.Reflectivity = packet.Blocks[blockIndex].Channels[j].Reflectivity
-		point.LastReflection = lastReturn
-		point.TimingOffset = timingOffsets[j][blockIndex]
-		s.SphericalPoints[blockIndex*ChannelsPerBlock+j] = point
 	}
 }
