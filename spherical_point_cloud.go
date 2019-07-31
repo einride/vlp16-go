8aeb780a2ba152b050a33073b972d0f66ffc0386
Oscar Söderlund <odsod@users.noreply.github.com>
2019-07-31 06:50:34 +0200

    Simplify SphericalPointCloud.UnmarshalPacket
    Functionally equivalent, but simpler. Covered by existing tests.

--- spherical_point_cloud.go
+++ spherical_point_cloud.go
@@ -21,32 +21,24 @@ type SphericalPoint struct {
 func (s *SphericalPointCloud) UnmarshalPacket(packet *Packet) {
 	// duration is in nanoseconds and Velodyne timestamp in microseconds
 	s.Timestamp = time.Duration(packet.Timestamp) * time.Microsecond
-	for i := 0; i < len(packet.Blocks); i++ {
-		azimuth := packet.Blocks[i].Azimuth
+	for i := range packet.Blocks {
+		block := &packet.Blocks[i]
+		azimuth := block.Azimuth
 		timingOffsets := calculateTimingOffset(packet.ReturnMode)
-		for j := 0; j < len(packet.Blocks[0].Channels); j++ {
+		for j := range block.Channels {
+			channel := &block.Channels[j]
 			if j == 16 {
 				azimuth = interpolateAzimuth(i, packet)
 			}
-			distance := packet.Blocks[i].Channels[j].Distance
-			lastReturn := false
-			switch packet.ReturnMode {
-			case ReturnModeDualReturn:
-				// Even number blocks (0,2,4,...) contain last return
-				if i%2 == 0 {
-					lastReturn = true
-				}
-			case ReturnModeLastReturn:
-				lastReturn = true
-			}
-			point := SphericalPoint{}
-			point.Distance = float64(distance) * distanceFactor
+			point := &s.SphericalPoints[i*ChannelsPerBlock+j]
+			point.Distance = float64(channel.Distance) * distanceFactor
 			point.Azimuth = deg2Rad(float64(azimuth) * azimuthFactor)
 			point.Elevation = verticalAngle(j)
 			point.Reflectivity = packet.Blocks[i].Channels[j].Reflectivity
-			point.LastReflection = lastReturn
+			point.LastReflection = packet.ReturnMode == ReturnModeLastReturn ||
+				// dual return mode: even number blocks (0,2,4,...) contain last return
+				packet.ReturnMode == ReturnModeDualReturn && i%2 == 0
 			point.TimingOffset = timingOffsets[j][i]
-			s.SphericalPoints[i*ChannelsPerBlock+j] = point
 		}
 	}
 }
