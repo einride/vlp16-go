bc76e97d091c30f4e9bf95b90c7115c084494cd8
Oscar Söderlund <odsod@users.noreply.github.com>
2020-12-29 10:44:20 +0100

    feat!: simplify API and use more idiomatic names
    Instead of setting the read deadline via context.Context, expose a
    SetReadDeadline method to make the API work more like a standard net
    connection.
    
    Rename vlp16.Receiver to vlp16.PacketListener.

--- packetlistener.go
+++ packetlistener.go
@@ -4,27 +4,33 @@ import (
 	"context"
 	"fmt"
 	"net"
+	"time"
 
 	"golang.org/x/net/ipv4"
 )
 
 // ListenUDP listens for VLP-16 UDP packets on the specified address.
-func ListenUDP(ctx context.Context, addr string, receiverOpts ...ReceiverOption) (*Receiver, error) {
-	opts := defaultReceiverOptions()
-	for _, receiverOpt := range receiverOpts {
+func ListenUDP(ctx context.Context, addr string, listenOpts ...ListenOption) (_ *PacketListener, err error) {
+	defer func() {
+		if err != nil {
+			err = fmt.Errorf("listen VLP-16 UDP packets: %w", err)
+		}
+	}()
+	opts := defaultListenOptions()
+	for _, receiverOpt := range listenOpts {
 		receiverOpt(opts)
 	}
 	var listenConfig net.ListenConfig
 	packetConn, err := listenConfig.ListenPacket(ctx, "udp4", addr)
 	if err != nil {
-		return nil, fmt.Errorf("VLP-16: listen UDP: %w", err)
+		return nil, err
 	}
 	udpConn := packetConn.(*net.UDPConn)
 	if err := udpConn.SetReadBuffer(opts.bufferSizeBytes); err != nil {
-		return nil, fmt.Errorf("VLP-16: listen UDP: %w", err)
+		return nil, err
 	}
 	conn := ipv4.NewPacketConn(udpConn)
-	c := &Receiver{conn: conn}
+	c := &PacketListener{conn: conn}
 	// allocate memory for batch reads
 	c.messages = make([]ipv4.Message, 0, opts.batchSize)
 	for i := 0; i < opts.batchSize; i++ {
@@ -36,8 +42,8 @@ func ListenUDP(ctx context.Context, addr string, receiverOpts ...ReceiverOption)
 	return c, nil
 }
 
-// Receiver receives VLP-16 packets.
-type Receiver struct {
+// PacketListener listens for VLP-16 packets.
+type PacketListener struct {
 	conn             *ipv4.PacketConn
 	messages         []ipv4.Message
 	rawPacketBuf     []*RawPacket
@@ -46,18 +52,14 @@ type Receiver struct {
 	currPacket       Packet
 }
 
-// Receive a VLP-16 packet.
-func (c *Receiver) Receive(ctx context.Context) error {
+// ReadPacket reads a VLP-16 packet.
+func (c *PacketListener) ReadPacket() error {
 	c.currMessageIndex++
 	if c.currMessageIndex >= c.messageBufSize {
 		c.currMessageIndex = 0
-		deadline, _ := ctx.Deadline()
-		if err := c.conn.SetReadDeadline(deadline); err != nil {
-			return fmt.Errorf("VLP-16 receiver: %w", err)
-		}
 		n, err := c.conn.ReadBatch(c.messages, 0)
 		if err != nil {
-			return fmt.Errorf("VLP-16 receiver: %w", err)
+			return fmt.Errorf("read VLP-16 packet: %w", err)
 		}
 		c.messageBufSize = n
 	}
@@ -65,25 +67,33 @@ func (c *Receiver) Receive(ctx context.Context) error {
 	return nil
 }
 
+// SetReadDeadline sets the read deadline associated with the listener's underlying UDP connection.
+func (c *PacketListener) SetReadDeadline(deadline time.Time) error {
+	if err := c.conn.SetReadDeadline(deadline); err != nil {
+		return fmt.Errorf("set VLP-16 listener read deadline: %w", err)
+	}
+	return nil
+}
+
 // SourceIP returns the source IP of the last received VLP-16 packet.
-func (c *Receiver) SourceIP() net.IP {
+func (c *PacketListener) SourceIP() net.IP {
 	return c.messages[c.currMessageIndex].Addr.(*net.UDPAddr).IP
 }
 
 // RawPacket returns the raw, unprocessed last received VLP-16 packet.
-func (c *Receiver) RawPacket() *RawPacket {
+func (c *PacketListener) RawPacket() *RawPacket {
 	return c.rawPacketBuf[c.currMessageIndex]
 }
 
 // Packet returns the last received VLP-16 packet.
-func (c *Receiver) Packet() *Packet {
+func (c *PacketListener) Packet() *Packet {
 	return &c.currPacket
 }
 
 // Close the client's underlying UDP connection.
-func (c *Receiver) Close() error {
+func (c *PacketListener) Close() error {
 	if err := c.conn.Close(); err != nil {
-		return fmt.Errorf("VLP-16 receiver: close: %w", err)
+		return fmt.Errorf("close VLP-16 listener: %w", err)
 	}
 	return nil
 }
